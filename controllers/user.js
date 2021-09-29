b4d3b06307abf49f4cfab4c0b28b44e6fffab2c4
Lance <lance.newman@accenture.com>
2021-09-29 10:31:07 -0400

    Done

--- controllers/user.js
+++ controllers/user.js
@@ -18,140 +18,17 @@ exports.count = async (req, res) => {}
 exports.create = async (req, res) => {}
 
 exports.findAll = async (req, res) => {
-    const { commentCount, page, q, sort, dir } = req.query
-
-    let limit = 20
-    let order = [
-        [db.Sequelize.col("commentCount"), "DESC"],
-        [db.Sequelize.col("name"), "ASC"]
-    ]
-    let where = {
-        // [Op.or]: [
-        name: {
-            [Op.like]: `%${q}%`
-        }
-        // [Op.or]: [
-        //    {
-        //        username: {
-        //            [Op.like]: `%${q}%`
-        //        }
-        //    }
-        // ]
-        // ]
-    }
-
-    if (typeof q === "undefined" || q === "") {
-        where = {}
-    }
-
-    if (typeof commentCount !== "undefined" && commentCount > 0) {
-        where.commentCount = {
-            [Op.gt]: commentCount
-        }
-    }
-
-    let attributes = [
-        [db.Sequelize.col("user.name"), "name"],
-        [db.Sequelize.col("user.id"), "id"],
-        [db.Sequelize.col("user.username"), "username"],
-        [
-            db.Sequelize.fn("COUNT", db.Sequelize.fn("DISTINCT", db.Sequelize.col("comments.id"))),
-            "commentCount"
-        ]
-    ]
-    let include = [
-        {
-            attributes: [],
-            model: Post,
-            required: false
-        },
-        {
-            attributes: [],
-            model: Comment,
-            required: false
-        }
-    ]
-    const offset = isNaN(page) ? 0 : page * limit
-
-    User.findAll({
-        attributes,
-        group: ["user.id"],
-        include,
-        limit,
-        offset,
-        order,
-        raw: true,
-        subQuery: false,
-        where
+    return res.status(500).send({
+        error: true,
+        msg: "Some error occurred"
     })
-        .then((users) => {
-            const hasMore = users.length === limit
-            return res.status(200).send({
-                error: false,
-                hasMore,
-                msg: "Success",
-                page: parseInt(page) + 1,
-                results: users
-            })
-        })
-        .catch((err) => {
-            const msg = err.message || "Some error occurred"
-            console.error("error msg", msg)
-            return res.status(200).send({
-                error: true,
-                msg
-            })
-        })
 }
 
 exports.findOne = async (req, res) => {
-    const { username } = req.params
-
-    User.findOne({
-        attributes: ["id", "email", "name", "username"],
-        raw: true,
-        where: {
-            username
-        }
+    return res.status(500).send({
+        error: true,
+        msg: "Some error occurred"
     })
-        .then(async (data) => {
-            // The user with the given username does not exist
-            if (data === null) {
-                return res.status(404).send({
-                    error: true,
-                    msg: "This user does not exist"
-                })
-            }
-
-            // Calculate comment counts
-            const commentCount = await Comment.count({
-                distinct: true,
-                where: {
-                    userId: data.id
-                }
-            }).then((count) => count)
-            const responseCount = await CommentResponse.count({
-                distinct: true,
-                where: {
-                    userId: data.id
-                }
-            }).then((count) => count)
-            data.commentCount = commentCount + responseCount
-
-            return res.status(200).send({
-                error: false,
-                msg: "success",
-                user: data
-            })
-        })
-        .catch((err) => {
-            const msg = err.message || "Some error occurred"
-            console.error("error msg", msg)
-            res.status(500).send({
-                error: true,
-                msg
-            })
-        })
 }
 
 exports.getUserComments = async (req, res) => {}
